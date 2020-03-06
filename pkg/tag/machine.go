48ff663d3cd9b818d22f0a80d9ba52be55f80bf9
Gerrit <gerrit.schwerthelm@f-i-ts.de>
2020-03-06 10:39:57 +0100

    Make it really simple now.
    Just put in the tags names, working with the tags must be done by the consumer.

---
+++ pkg/tag/machine.go
@@ -0,0 +1,16 @@
+package tag
+
+const (
+	// MachineProject tag to store the project where the machine belongs to
+	MachineProject = "machine.metal-stack.io/project"
+	// MachineDescription tag to store machine description
+	MachineDescription = "machine.metal-stack.io/description"
+	// MachineID tag to store machine ID
+	MachineID = "machine.metal-stack.io/id"
+	// MachineName tag to store machine name
+	MachineName = "machine.metal-stack.io/name"
+	// MachineTenant tag to store the tenant the machine belongs to
+	MachineTenant = "machine.metal-stack.io/tenant"
+	// MachineNetworkPrimaryASN tag to store the primary BGP ASN the machine announces.
+	MachineNetworkPrimaryASN = "machine.metal-stack.io/network.primary.asn"
+)
