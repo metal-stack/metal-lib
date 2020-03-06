c5efd5be6ca85ed9b5f91530af1389e74518d4b0
Stefan Majer <stefan.majer@gmail.com>
2020-03-06 11:04:46 +0100

    Define tags according to the current state (#5)
    * Define tags according to the current state
    
    * Save broken work
    
    * Make it really simple now.
    
    Just put in the tags names, working with the tags must be done by the consumer.
    
    * Last improvements.
    
    * Review.
    
    Co-authored-by: Sandro Koll <sandro.koll@f-i-ts.de>
    Co-authored-by: Gerrit <gerrit.schwerthelm@f-i-ts.de>

---
+++ pkg/tag/cluster.go
@@ -0,0 +1,16 @@
+package tag
+
+const (
+	// ClusterID tag to store the id of the cluster
+	ClusterID = "cluster.metal-stack.io/id"
+	// ClusterName tag to store the name of the cluster
+	ClusterName = "cluster.metal-stack.io/name"
+	// ClusterDescription tag to store the description of the cluster
+	ClusterDescription = "cluster.metal-stack.io/description"
+	// ClusterProject tag to store the project the cluster belongs to
+	ClusterProject = "cluster.metal-stack.io/project"
+	// ClusterTenant tag to store the tenant of the cluster
+	ClusterTenant = "cluster.metal-stack.io/tenant"
+	// ClusterServiceFQN tag to identify a service running in the cluster
+	ClusterServiceFQN = "cluster.metal-stack.io/id/namespace/service"
+)
