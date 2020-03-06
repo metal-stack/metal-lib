b2f6ff0798fd4a94b4c7bd5aa929962d8f01399c
Gerrit <gerrit.schwerthelm@f-i-ts.de>
2020-03-06 11:00:11 +0100

    Last improvements.

--- pkg/tag/cluster.go
+++ pkg/tag/cluster.go
@@ -1,16 +1,16 @@
 package tag
 
 const (
-	// ClusterProject tag to store the project the cluster belongs to
-	ClusterProject = "cluster.metal-stack.io/project"
-	// ClusterDescription tag to store the description of the cluster
-	ClusterDescription = "cluster.metal-stack.io/description"
 	// ClusterID tag to store the id of the cluster
 	ClusterID = "cluster.metal-stack.io/id"
 	// ClusterName tag to store the name of the cluster
 	ClusterName = "cluster.metal-stack.io/name"
+	// ClusterDescription tag to store the description of the cluster
+	ClusterDescription = "cluster.metal-stack.io/description"
+	// ClusterProject tag to store the project the cluster belongs to
+	ClusterProject = "cluster.metal-stack.io/project"
 	// ClusterTenant tag to store the tenant of the cluster
 	ClusterTenant = "cluster.metal-stack.io/tenant"
-	// ClusterServiceFQN the prefix of the tag used to identify services
-	ClusterServiceFQN = "cluster.metal-stack.io/id/service/namespace"
+	// ClusterServiceFQN tag to identify a services running in the cluster
+	ClusterServiceFQN = "cluster.metal-stack.io/id/namespace/service"
 )
--- pkg/tag/machine.go
+++ pkg/tag/machine.go
@@ -1,14 +1,14 @@
 package tag
 
 const (
-	// MachineProject tag to store the project where the machine belongs to
-	MachineProject = "machine.metal-stack.io/project"
-	// MachineDescription tag to store machine description
-	MachineDescription = "machine.metal-stack.io/description"
 	// MachineID tag to store machine ID
 	MachineID = "machine.metal-stack.io/id"
 	// MachineName tag to store machine name
 	MachineName = "machine.metal-stack.io/name"
+	// MachineDescription tag to store machine description
+	MachineDescription = "machine.metal-stack.io/description"
+	// MachineProject tag to store the project where the machine belongs to
+	MachineProject = "machine.metal-stack.io/project"
 	// MachineTenant tag to store the tenant the machine belongs to
 	MachineTenant = "machine.metal-stack.io/tenant"
 	// MachineNetworkPrimaryASN tag to store the primary BGP ASN the machine announces.
