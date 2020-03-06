c53df80bdacd2fbe143174fd1497399c2c272541
Stefan Majer <stefan.majer@f-i-ts.de>
2020-03-06 10:27:46 +0100

    Save broken work

--- pkg/tag/tags.go
+++ pkg/tag/tags.go
@@ -6,43 +6,40 @@ import (
 )
 
 const (
-	clusterPrefix = "cluster.metal-pod.io" //TODO replace with "cluster.metal-stack.io"
-	machinePrefix = "machine.metal-pod.io" //TODO replace with "machine.metal-stack.io"
-)
+	// MachineProject tag to store the project where the machine belongs to
+	MachineProject = "machine.metal-stack.io/project"
+	// MachineDescription tag to store machine description
+	MachineDescription = "machine.metal-stack.io/description"
+	// MachineID tag to store machine ID
+	MachineID = "machine.metal-stack.io/id"
+	// MachineName tag to store machine name
+	MachineName = "machine.metal-stack.io/name"
+	// MachineTenant tag to store the tenant the machine belongs to
+	MachineTenant = "machine.metal-stack.io/tenant"
+	// MachineNetworkPrimaryASN tag to store the primary BGP ASN the machine announces.
+	MachineNetworkPrimaryASN = "machine.metal-stack.io/network.primary.asn"
 
-const (
-	// ClusterQualifier identifies the cluster
-	ClusterQualifier = "clusterid"
+	// ClusterProject tag to store the project the cluster belongs to
+	ClusterProject = "cluster.metal-stack.io/project"
+	// ClusterDescription tag to store the description of the cluster
+	ClusterDescription = "cluster.metal-stack.io/description"
+	// ClusterID tag to store the id of the cluster
+	ClusterID = "cluster.metal-stack.io/id"
+	// ClusterName tag to store the name of the cluster
+	ClusterName = "cluster.metal-stack.io/name"
+	// ClusterTenant tag to store the tenant of the cluster
+	ClusterTenant = "cluster.metal-stack.io/tenant"
+	// ClusterServiceFQN the prefix of the tag used to identify services
+	ClusterServiceFQN = "cluster.metal-stack.io/" + IDQualifier + "/" + NamespaceQualifier + "/" + ServiceQualifier
+
+	// IDQualifier identifies the cluster or machine
+	IDQualifier = "id"
 
 	// ServiceQualifier identifies the service
 	ServiceQualifier = "service"
 
 	// NamespaceQualifier identifies the namespace
 	NamespaceQualifier = "namespace"
-
-	// ClusterPrefix the prefix of the tag used to identify a cluster
-	ClusterPrefix = clusterPrefix + "/" + ClusterQualifier //TODO either remove clusterPrefix or ClusterPrefix
-
-	// ServicePrefix the prefix of the tag used to identify services
-	ServicePrefix = ClusterPrefix + "/" + NamespaceQualifier + "/" + ServiceQualifier
-
-	// MachineQualifier identifies the machine
-	MachineQualifier = "machineid"
-
-	// MachinePrefix the prefix of the tag used to identify a machine
-	MachinePrefix = machinePrefix + "/" + MachineQualifier   //TODO either machinePrefix or MachinePrefix
-	MetalPrefix   = "metal.metal-pod.io/" + MachineQualifier //TODO remove entirely. Use MachinePrefix instead
-
-	// PrimaryNetworkASN the primary network ASN tag
-	PrimaryNetworkASN = machinePrefix + "/" + "network.primary.asn"
-
-	ClusterProject     = clusterPrefix + "/project"
-	ClusterDescription = clusterPrefix + "/description"
-	ClusterID          = clusterPrefix + "/id"
-	ClusterName        = clusterPrefix + "/name"
-	ClusterTenant      = clusterPrefix + "/tenant"
-
-	MachineProjectID = machinePrefix + "/project-id" //TODO remove entirely. Use ClusterProject instead
 )
 
 // ServiceTag constructs the service tag for the given cluster and service
@@ -52,7 +49,7 @@ func ServiceTag(clusterID string, namespace, serviceName string) string {
 
 // ServiceTagClusterPrefix constructs the prefix of the service tag that identify all services of a cluster
 func ServiceTagClusterPrefix(clusterID string) string {
-	return fmt.Sprintf("%s=%s", ServicePrefix, clusterID)
+	return fmt.Sprintf("%s=%s", ClusterServiceFQN, clusterID)
 }
 
 // IsMachine returns true if the given tag is a machine-tag.
