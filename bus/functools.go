6ac205603e1b20053b1a5c34ab174e9673de101c
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2020-03-20 12:32:27 +0100

    use plain function args for introspection

--- bus/functools.go
+++ bus/functools.go
@@ -1,6 +1,11 @@
 package bus
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"time"
+)
 
 // Endpoints couples a consumer and a publisher to a single entity.
 type Endpoints struct {
@@ -16,8 +21,10 @@ func NewEndpoints(consumer *Consumer, publisher Publisher) *Endpoints {
 	}
 }
 
-// A Func is a function with an argument which can return an error when it fails.
-type Func func(interface{}) error
+// DirectEndpoints returns endpoints which call the target function directly.
+func DirectEndpoints() *Endpoints {
+	return &Endpoints{}
+}
 
 // A Function encapsulates a Func which can be called with an argument. The invocation will be delegated through
 // nsq so multiple instances of the same function can run in different processes. Only one of them
@@ -25,18 +32,36 @@ type Func func(interface{}) error
 type Function struct {
 	endpoints *Endpoints
 	cr        *ConsumerRegistration
-	fn        Func
+	fn        reflect.Value
 	name      string
-}
-
-type funcParam struct {
-	Parameter interface{} `json:"parameter"`
 }
 
 // Function creates a Function from the with the given endpoints. The name of the function will be a
 // distributed selector for the given go function. So every function which is registered with the same
 // name can receive the invocation inside the cluster.
-func (e *Endpoints) Function(name string, fn Func) (*Function, error) {
+func (e *Endpoints) Function(name string, fn interface{}) (*Function, error) {
+	fntype := reflect.TypeOf(fn)
+	if fntype.Kind() != reflect.Func {
+		return nil, fmt.Errorf("the function parameter must be a function")
+	}
+	if fntype.NumIn() != 1 {
+		return nil, fmt.Errorf("the number of parameters in the function must be one")
+	}
+	if fntype.NumOut() != 1 {
+		return nil, fmt.Errorf("the function must return exactly one value of type error")
+	}
+	errtype := reflect.TypeOf(errors.New(""))
+	if !errtype.AssignableTo(fntype.Out(0)) {
+		return nil, fmt.Errorf("the return type is not of type 'error'")
+	}
+
+	if e.consumer == nil || e.publisher == nil {
+		// someone wants a sync function
+		return &Function{name: name, fn: reflect.ValueOf(fn)}, nil
+	}
+	if err := e.publisher.CreateTopic(name); err != nil {
+		return nil, fmt.Errorf("cannot create topic: %q: %w", name, err)
+	}
 	reg, err := e.consumer.Register(name, "function#ephemeral")
 	if err != nil {
 		return nil, fmt.Errorf("cannot register consumer for function %q: %w", name, err)
@@ -44,22 +69,54 @@ func (e *Endpoints) Function(name string, fn Func) (*Function, error) {
 	cb := &Function{
 		endpoints: e,
 		cr:        reg,
-		fn:        fn,
+		fn:        reflect.ValueOf(fn),
 		name:      name,
 	}
-	if err = reg.Consume(funcParam{}, cb.receive, 5); err != nil {
+	partype := fntype.In(0)
+	for partype.Kind() == reflect.Ptr {
+		partype = partype.Elem()
+	}
+	pvalue := reflect.New(partype).Elem()
+	if err = reg.Consume(pvalue.Interface(), cb.receive, 5); err != nil {
 		return nil, fmt.Errorf("cannot consume: %w", err)
 	}
 	return cb, nil
 }
 
 func (f *Function) receive(par interface{}) error {
-	fp := par.(*funcParam)
-	return f.fn(fp.Parameter)
+	v := reflect.ValueOf(par)
+	vkind := reflect.TypeOf(par).Kind()
+	pkind := f.fn.Type().In(0).Kind()
+
+	parms := []reflect.Value{v}
+	if vkind != pkind {
+		if pkind == reflect.Ptr {
+			// function wants a ptr
+			parms = []reflect.Value{v.Addr()}
+		} else if vkind == reflect.Ptr {
+			// function wants value
+			parms = []reflect.Value{v.Elem()}
+		}
+	}
+	res := f.fn.Call(parms)
+	if res[0].IsNil() {
+		return nil
+	}
+	return res[0].Interface().(error)
 }
 
-// MustSucceed invokes the function with no limit. So nsq will invoke the connected go function
-// until no error is returned.
-func (f *Function) MustSucceed(arg interface{}) error {
-	return f.endpoints.publisher.Publish(f.name, funcParam{Parameter: arg})
+// Must invokes the function with no limit. So nsq will invoke the connected go function
+// until no error is returned. The function itself returns an error if there is a
+// communication problem with nsq.
+func (f *Function) Must(arg interface{}) error {
+	if f.endpoints == nil {
+		//sync function
+		for {
+			if err := f.receive(arg); err == nil {
+				return nil
+			}
+			time.Sleep(time.Millisecond * 100)
+		}
+	}
+	return f.endpoints.publisher.Publish(f.name, arg)
 }
--- bus/functools_test.go
+++ bus/functools_test.go
@@ -6,14 +6,52 @@ import (
 	"testing"
 )
 
+func TestFunctionWithWrongParams(t *testing.T) {
+	e := NewEndpoints(consumer, publisher)
+	_, err := e.Function("helloworld", func(arg1, arg2 string) error {
+		return nil
+	})
+	if err == nil {
+		t.Errorf("function creation should fail: must only have one parameter")
+	}
+}
+
+func TestFunctionWithWrongResults(t *testing.T) {
+	e := NewEndpoints(consumer, publisher)
+	_, err := e.Function("helloworld", func(arg1 string) (string, error) {
+		return "", nil
+	})
+	if err == nil {
+		t.Errorf("function creation should fail: must only return one result")
+	}
+}
+
+func TestFunctionWithWrongResult(t *testing.T) {
+	e := NewEndpoints(consumer, publisher)
+	_, err := e.Function("helloworld", func(arg1 string) string {
+		return ""
+	})
+	if err == nil {
+		t.Errorf("function creation should fail: must return an error")
+	}
+}
+
+func TestFunctionWithWrongFunc(t *testing.T) {
+	e := NewEndpoints(consumer, publisher)
+	_, err := e.Function("helloworld", struct{}{})
+	if err == nil {
+		t.Errorf("function creation should fail: must get a function as parameter")
+	}
+}
+
 func TestFunctionHelloWorld(t *testing.T) {
 	var wg sync.WaitGroup
 	wg.Add(1)
 
 	res := ""
 	e := NewEndpoints(consumer, publisher)
-	f, err := e.Function("helloworld", func(arg interface{}) error {
-		res = fmt.Sprintf("%v", arg)
+	f, err := e.Function("helloworld", func(arg string) error {
+		res = arg
 		wg.Done()
 		return nil
 	})
@@ -22,7 +60,7 @@ func TestFunctionHelloWorld(t *testing.T) {
 	}
 
 	value := "Hello world"
-	err = f.MustSucceed(value)
+	err = f.Must(value)
 	wg.Wait()
 
 	if err != nil {
@@ -42,12 +80,12 @@ func TestFunctionReplay(t *testing.T) {
 	num := 0
 	retries := 1
 	e := NewEndpoints(consumer, publisher)
-	f, err := e.Function("helloworld-replay", func(arg interface{}) error {
+	f, err := e.Function("helloworld-replay", func(arg string) error {
 		if num < retries {
 			num += 1
 			return fmt.Errorf("not on the first run: %d", num)
 		}
-		res = fmt.Sprintf("%v: %d", arg, num)
+		res = fmt.Sprintf("%s: %d", arg, num)
 		wg.Done()
 		return nil
 	})
@@ -56,7 +94,7 @@ func TestFunctionReplay(t *testing.T) {
 	}
 
 	value := "Hello world"
-	err = f.MustSucceed(value)
+	err = f.Must(value)
 	wg.Wait()
 
 	if err != nil {
@@ -64,6 +102,66 @@ func TestFunctionReplay(t *testing.T) {
 	}
 
 	if res != fmt.Sprintf("%s: %d", value, retries) {
+		t.Errorf("result is %q, but should be %q", res, value)
+	}
+}
+
+type testStruct struct {
+	Name string
+}
+
+func TestFunctionWithStruct(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	res := ""
+	e := NewEndpoints(consumer, publisher)
+	f, err := e.Function("hellostruct", func(arg *testStruct) error {
+		res = arg.Name
+		wg.Done()
+		return nil
+	})
+	if err != nil {
+		t.Errorf("cannot create function, %v", err)
+	}
+
+	value := "Hello world"
+	err = f.Must(testStruct{Name: value})
+	wg.Wait()
+
+	if err != nil {
+		t.Fatalf("function must succeed, %v", err)
+	}
+
+	if res != value {
+		t.Errorf("result is %q, but should be %q", res, value)
+	}
+}
+
+func TestDirectFunctionWithStruct(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	res := ""
+	e := DirectEndpoints()
+	f, err := e.Function("direct-hellostruct", func(arg *testStruct) error {
+		res = arg.Name
+		wg.Done()
+		return nil
+	})
+	if err != nil {
+		t.Errorf("cannot create function, %v", err)
+	}
+
+	value := "Hello world"
+	err = f.Must(&testStruct{Name: value})
+	wg.Wait()
+
+	if err != nil {
+		t.Fatalf("function must succeed, %v", err)
+	}
+
+	if res != value {
 		t.Errorf("result is %q, but should be %q", res, value)
 	}
 }
