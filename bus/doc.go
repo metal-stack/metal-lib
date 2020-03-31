5f3b32b8ca82f8b3d8ec984ab9c08f99311b7e71
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2020-03-31 09:11:16 +0200

    add some docs

--- bus/doc.go
+++ bus/doc.go
@@ -22,5 +22,33 @@
 
   If the process with the unique function ends, the topic and the channels will be removed from nsq
   because they are ephemeral.
+
+  Creating a named function
+
+     ep := NewEndpoints(...)
+     f, err := ep.Function("hello-service", func (s string) error {
+        fmt.Printf("Hello %s\n", s)
+        return nil
+     })
+     f("world"); // prints "Hello world"
+
+  creates a client and server for the function, depending on the endpoint. If there is a consumer in
+  the endpoint, a server is registered; if there is a publisher a client is also created. So using an
+  endpoint with consumer and publisher creates a function which will be hosted by the same process which
+  creates the function.
+
+  When you only need a client for a wellknown function, use the `Client` function:
+
+    ep := NewEndpoints(...)
+    f, err := ep.Client("hello-service")
+    f("world")
+
+  In this case there should be another process which registered a consuming function.
+
+  Last but not least you can use `Unique` to create a unique consumer which only exists as long
+  as the process exists which did the registration. Unique consumers return their name and they
+  can be used to transport responses from well known services back to clients. The client has
+  to register a unique consumer and pass the name of this function to the service which will post
+  back the response back to the client.
 */
 package bus
