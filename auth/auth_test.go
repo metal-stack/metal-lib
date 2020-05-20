9b0eaf2422e2ea1a10fccbb2db57a9a511520e0a
abloss <8138505+droid42@users.noreply.github.com>
2020-05-20 11:19:01 +0200

    Implements #10 kubeconfig/auth: let client-apps choose contextName for storing credentials (#11)
    * Implements #10 kubeconfig/auth: let client-apps choose contextName for storing credentials
    
    * review findings

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -1,8 +1,15 @@
 package auth
 
 import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"log"
+	"os"
 	"testing"
 )
+
+const testCloudContextName123 = "ctx123"
 
 func Test_IssuerValidation(t *testing.T) {
 	err := OIDCFlow(Config{})
@@ -39,4 +46,58 @@ func Test_TokenHandlerValidation(t *testing.T) {
 	if err == nil {
 		t.Fatal("Expected err")
 	}
+}
+
+func Test_NewUpdateKubeConfigHandler(t *testing.T) {
+	tokenInfo := TokenInfo{
+		IDToken:      "123",
+		RefreshToken: "456",
+		TokenClaims:  claim{},
+		IssuerConfig: IssuerConfig{},
+	}
+
+	file, err := ioutil.TempFile("", "prefix")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	var b bytes.Buffer
+	thf := NewUpdateKubeConfigHandler(file.Name(), &b)
+	err = thf(tokenInfo)
+	assert.NoError(t, err)
+
+	_, err = GetAuthContext(file.Name(), "xyz")
+	assert.EqualError(t, err, "no contexts, name=xyz found")
+
+	authCtx, err := GetAuthContext(file.Name(), cloudContext)
+	assert.NoError(t, err)
+	assert.Equal(t, "123", authCtx.IDToken)
+}
+
+func Test_NewUpdateKubeConfigHandlerWithContext(t *testing.T) {
+	tokenInfo := TokenInfo{
+		IDToken:      "123",
+		RefreshToken: "456",
+		TokenClaims:  claim{},
+		IssuerConfig: IssuerConfig{},
+	}
+
+	file, err := ioutil.TempFile("", "prefix")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	var b bytes.Buffer
+	thf := NewUpdateKubeConfigHandler(file.Name(), &b, WithContextName("ctx123"))
+	err = thf(tokenInfo)
+	assert.NoError(t, err)
+
+	_, err = GetAuthContext(file.Name(), "cloudctl-xyz")
+	assert.EqualError(t, err, "no contexts, name=cloudctl-xyz found")
+
+	authCtx, err := GetAuthContext(file.Name(), testCloudContextName123)
+	assert.NoError(t, err)
+	assert.Equal(t, "123", authCtx.IDToken)
 }
