d042578b2dd98b74592da1210fc435e2084d0aab
Gerrit <tirreg91@googlemail.com>
2020-09-09 13:24:40 +0200

    Add default external network tag.

---
+++ pkg/tag/network.go
@@ -0,0 +1,6 @@
+package tag
+
+const (
+	// NetworkDefaultExternal indicates a network that can serve as a default for IP allocations
+	NetworkDefaultExternal = "network.metal-stack.io/default-external"
+)
