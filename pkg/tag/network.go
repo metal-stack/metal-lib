6d6cf9f9293053ed15534cb36f64f0cace3fa2e8
Gerrit <tirreg91@googlemail.com>
2020-09-17 09:50:01 +0200

    Add default network tag.

--- pkg/tag/network.go
+++ pkg/tag/network.go
@@ -1,6 +1,9 @@
 package tag
 
 const (
+	// NetworkDefault indicates a network that can serve as a default network for cluster creation
+	// there should only be one default network in a metal control plane, otherwise behavior will be non-deterministic
+	NetworkDefault = "network.metal-stack.io/default"
 	// NetworkDefaultExternal indicates a network that can serve as a default for IP allocations
 	NetworkDefaultExternal = "network.metal-stack.io/default-external"
 )
