02d782170b6a41db6080705ebe3214e812fbe68b
Gerrit <info@gerritschwerthelm.de>
2022-07-07 13:50:06 +0200

    Provide common cli library for metal-stack CLIs.

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/metal-stack/v v1.0.3
 	github.com/nsqio/go-nsq v1.1.0
 	github.com/nsqio/nsq v1.2.1
+	github.com/spf13/afero v1.8.2
 	github.com/stretchr/testify v1.7.4
 	go.uber.org/zap v1.21.0
 	golang.org/x/oauth2 v0.0.0-20220622183110-fd043fe589d2
@@ -58,11 +59,13 @@ require (
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	go.mongodb.org/mongo-driver v1.9.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/net v0.0.0-20220622184535-263ec571b305 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
---
+++ pkg/cli/applier_test.go
@@ -0,0 +1,155 @@
+package cli
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/require"
+)
+
+type testYAML struct {
+	ID     string
+	Labels []string
+}
+
+var (
+	testYAMLRaw = `---
+id: a
+labels:
+  - a
+---
+id: b
+labels:
+  - b
+`
+	errorComparer = cmp.Comparer(func(x, y error) bool {
+		if x == nil && y == nil {
+			return true
+		}
+		if x == nil && y != nil {
+			return false
+		}
+		if x != nil && y == nil {
+			return false
+		}
+		return x.Error() == y.Error()
+	})
+)
+
+func Test_readYAML(t *testing.T) {
+	const testFile = "/test.yaml"
+
+	tests := []struct {
+		name    string
+		mockFn  func(fs afero.Fs)
+		want    []*testYAML
+		wantErr error
+	}{
+		{
+			name: "parsing empty file",
+			mockFn: func(fs afero.Fs) {
+				require.NoError(t, afero.WriteFile(fs, testFile, []byte(""), 0755))
+			},
+			want: nil,
+		},
+		{
+			name: "parsing multi-document yaml",
+			mockFn: func(fs afero.Fs) {
+				require.NoError(t, afero.WriteFile(fs, testFile, []byte(testYAMLRaw), 0755))
+			},
+			want: []*testYAML{
+				{
+					ID:     "a",
+					Labels: []string{"a"},
+				},
+				{
+					ID:     "b",
+					Labels: []string{"b"},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			fs := afero.NewMemMapFs()
+			if tt.mockFn != nil {
+				tt.mockFn(fs)
+			}
+
+			got, err := readYAML[*testYAML](fs, testFile)
+
+			if diff := cmp.Diff(tt.wantErr, err, errorComparer); diff != "" {
+				t.Errorf("error diff (+got -want):\n %s", diff)
+			}
+
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("diff (+got -want):\n %s", diff)
+			}
+		})
+	}
+}
+
+func Test_readYAMLIndex(t *testing.T) {
+	const testFile = "/test.yaml"
+
+	tests := []struct {
+		name    string
+		mockFn  func(fs afero.Fs)
+		index   int
+		want    *testYAML
+		wantErr error
+	}{
+		{
+			name: "request zero index",
+			mockFn: func(fs afero.Fs) {
+				require.NoError(t, afero.WriteFile(fs, testFile, []byte(testYAMLRaw), 0755))
+			},
+			index: 0,
+			want: &testYAML{
+				ID:     "a",
+				Labels: []string{"a"},
+			},
+		},
+		{
+			name: "request one index",
+			mockFn: func(fs afero.Fs) {
+				require.NoError(t, afero.WriteFile(fs, testFile, []byte(testYAMLRaw), 0755))
+			},
+			index: 1,
+			want: &testYAML{
+				ID:     "b",
+				Labels: []string{"b"},
+			},
+		},
+		{
+			name: "not existing index",
+			mockFn: func(fs afero.Fs) {
+				require.NoError(t, afero.WriteFile(fs, testFile, []byte(testYAMLRaw), 0755))
+			},
+			index:   2,
+			wantErr: fmt.Errorf("index not found in document: 2"),
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			fs := afero.NewMemMapFs()
+			if tt.mockFn != nil {
+				tt.mockFn(fs)
+			}
+
+			got, err := readYAMLIndex[*testYAML](fs, testFile, tt.index)
+
+			if diff := cmp.Diff(tt.wantErr, err, errorComparer); diff != "" {
+				t.Errorf("error diff (+got -want):\n %s", diff)
+			}
+
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("diff (+got -want):\n %s", diff)
+			}
+		})
+	}
+}
