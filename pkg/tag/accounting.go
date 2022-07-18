3830ed11559a38ce9fe8742d21e503bd908f09d5
Gerrit <info@gerritschwerthelm.de>
2022-07-18 11:46:57 +0200

    Add volume encryption tag.

--- pkg/tag/accounting.go
+++ pkg/tag/accounting.go
@@ -13,6 +13,8 @@ const (
 	AccountingNetworkTrafficInternal = "accounting.metal-stack.io/network-traffic-internal"
 	// AccountingVolumeReplicas tag to store accounting volume replicas
 	AccountingVolumeReplicas = "accounting.metal-stack.io/volume-replicas"
+	// AccountingVolumeEncryption tag to store accounting volume encryption information
+	AccountingVolumeEncryption = "accounting.metal-stack.io/volume-encryption"
 )
 
 // AccountingTags returns all accounting tags
@@ -24,5 +26,6 @@ func AccountingTags() map[string]bool {
 		AccountingNetworkTrafficExternal: true,
 		AccountingNetworkTrafficInternal: true,
 		AccountingVolumeReplicas:         true,
+		AccountingVolumeEncryption:       true,
 	}
 }
