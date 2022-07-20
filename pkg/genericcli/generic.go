0699f3abebba772cdc68565c962b90ad79f1a1d2
Gerrit <info@gerritschwerthelm.de>
2022-07-20 09:52:06 +0200

    Describe CUR.

--- pkg/genericcli/generic.go
+++ pkg/genericcli/generic.go
@@ -3,6 +3,10 @@ package genericcli
 import "github.com/spf13/afero"
 
 // GenericCLI can be used to gain generic CLI functionality.
+//
+// C is the create request for an entity.
+// U is the update request for an entity.
+// R is the response object of an entity.
 type GenericCLI[C any, U any, R any] struct {
 	fs           afero.Fs
 	crud         CRUD[C, U, R]
@@ -11,6 +15,10 @@ type GenericCLI[C any, U any, R any] struct {
 }
 
 // CRUD must be implemented in order to get generic CLI functionality.
+//
+// C is the create request for an entity.
+// U is the update request for an entity.
+// R is the response object of an entity.
 type CRUD[C any, U any, R any] interface {
 	// Get returns the entity with the given id.
 	Get(id string) (R, error)
@@ -25,6 +33,10 @@ type CRUD[C any, U any, R any] interface {
 }
 
 // NewGenericCLI returns a new generic cli.
+//
+// C is the create request for an entity.
+// U is the update request for an entity.
+// R is the response object of an entity.
 func NewGenericCLI[C any, U any, R any](crud CRUD[C, U, R]) *GenericCLI[C, U, R] {
 	fs := afero.NewOsFs()
 	return &GenericCLI[C, U, R]{
