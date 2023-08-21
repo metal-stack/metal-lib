190419c780732ed631c09a929385c56462d47f1b
Gerrit <Gerrit91@users.noreply.github.com>
2023-08-21 11:09:56 +0200

    Add `Get` function to tag helpers. (#103)

--- pkg/tag/helpers.go
+++ pkg/tag/helpers.go
@@ -1,10 +1,18 @@
 package tag
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 type TagMap map[string]string
 
-// NewLabelMap constructs a map of a list of labels.
+// New returns a new tag in the format key=value
+func New(key, value string) string {
+	return fmt.Sprintf("%s=%s", key, value)
+}
+
+// NewTagMap constructs a map of a list of labels.
 func NewTagMap(labels []string) TagMap {
 	result := TagMap{}
 	for _, l := range labels {
@@ -14,17 +22,27 @@ func NewTagMap(labels []string) TagMap {
 	return result
 }
 
-// Contains returns true when the given tag is contained in the label map.
-func (tm TagMap) Contains(tag, value string) bool {
-	v, ok := tm[tag]
+// Contains returns true when the given key is contained in the label map.
+func (tm TagMap) Contains(key, value string) bool {
+	v, ok := tm[key]
 	if !ok {
 		return false
 	}
 	return v == value
 }
 
-// Value returns true when the label map contains the given tag and returns the corresponding value.
-func (tm TagMap) Value(tag string) (string, bool) {
-	value, ok := tm[tag]
+// Value returns true when the label map contains the given key and returns the corresponding value.
+func (tm TagMap) Value(key string) (string, bool) {
+	value, ok := tm[key]
 	return value, ok
+}
+
+// Get returns the whole tag when the given key is contained in the label map.
+func (tm TagMap) Get(key string) (string, error) {
+	value, ok := tm.Value(key)
+	if !ok {
+		return "", fmt.Errorf("no tag with key %q found", key)
+	}
+
+	return New(key, value), nil
 }
