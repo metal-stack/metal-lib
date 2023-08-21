ec0230c1bb0557a2f7800ee939bd580cf9437514
Gerrit <info@gerritschwerthelm.de>
2023-08-21 10:08:46 +0200

    Add `Get` function to tag helpers.

--- pkg/tag/helpers_test.go
+++ pkg/tag/helpers_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/metal-stack/metal-lib/pkg/testcommon"
 )
 
 func TestTagMap_Contains(t *testing.T) {
@@ -72,14 +73,14 @@ func TestTagMap_Value(t *testing.T) {
 	tests := []struct {
 		name      string
 		labels    []string
-		tag       string
+		key       string
 		want      bool
 		wantValue string
 	}{
 		{
 			name:      "empty map",
 			labels:    nil,
-			tag:       ClusterID,
+			key:       ClusterID,
 			want:      false,
 			wantValue: "",
 		},
@@ -91,7 +92,7 @@ func TestTagMap_Value(t *testing.T) {
 				fmt.Sprintf("%s=%s", ClusterID, "test"),
 				fmt.Sprintf("%s=%s", ClusterName, "test cluster"),
 			},
-			tag:       ClusterID,
+			key:       ClusterID,
 			want:      true,
 			wantValue: "test",
 		},
@@ -102,7 +103,7 @@ func TestTagMap_Value(t *testing.T) {
 				fmt.Sprintf("%s=%s", ClusterEgress, "1.2.3.4"),
 				fmt.Sprintf("%s=%s", ClusterName, "test cluster"),
 			},
-			tag:       ClusterID,
+			key:       ClusterID,
 			want:      false,
 			wantValue: "",
 		},
@@ -111,12 +112,66 @@ func TestTagMap_Value(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			tm := NewTagMap(tt.labels)
-			gotValue, got := tm.Value(tt.tag)
+			gotValue, got := tm.Value(tt.key)
 			if got != tt.want {
 				t.Errorf("TagMap.Value() = %v, want %v", got, tt.want)
 			}
 			if diff := cmp.Diff(gotValue, tt.wantValue); diff != "" {
 				t.Errorf("TagMap.Value() diff = %s", diff)
+			}
+		})
+	}
+}
+
+func TestTagMap_Get(t *testing.T) {
+	tests := []struct {
+		name      string
+		labels    []string
+		key       string
+		wantValue string
+		wantErr   error
+	}{
+		{
+			name:      "empty map",
+			labels:    nil,
+			key:       ClusterID,
+			wantValue: "",
+			wantErr:   fmt.Errorf(`no tag with key "cluster.metal-stack.io/id" found`),
+		},
+		{
+			name: "get value",
+			labels: []string{
+				"label-with-no-assignment",
+				fmt.Sprintf("%s=%s", ClusterEgress, "1.2.3.4"),
+				fmt.Sprintf("%s=%s", ClusterID, "test"),
+				fmt.Sprintf("%s=%s", ClusterName, "test cluster"),
+			},
+			key:       ClusterID,
+			wantValue: fmt.Sprintf("%s=%s", ClusterID, "test"),
+		},
+		{
+			name: "not contained",
+			labels: []string{
+				"label-with-no-assignment",
+				fmt.Sprintf("%s=%s", ClusterEgress, "1.2.3.4"),
+				fmt.Sprintf("%s=%s", ClusterName, "test cluster"),
+			},
+			key:       ClusterID,
+			wantValue: "",
+			wantErr:   fmt.Errorf(`no tag with key "cluster.metal-stack.io/id" found`),
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			tm := NewTagMap(tt.labels)
+			gotValue, err := tm.Get(tt.key)
+			if diff := cmp.Diff(tt.wantErr, err, testcommon.ErrorStringComparer()); diff != "" {
+				t.Errorf("error diff (+got -want):\n %s", diff)
+			}
+
+			if diff := cmp.Diff(gotValue, tt.wantValue); diff != "" {
+				t.Errorf("TagMap.Get() diff = %s", diff)
 			}
 		})
 	}
