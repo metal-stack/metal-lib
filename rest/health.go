903108532c9d772d04925450a85ac4366f766175
qrnvttrl <quirin.vetterl@gmx.de>
2024-02-23 14:08:07 +0100

    Add Partitions to HealthResult and Check

--- rest/health.go
+++ rest/health.go
@@ -32,7 +32,7 @@ type HealthCheck interface {
 	// ServiceName returns the name of the service that is health checked.
 	ServiceName() string
 	// Check is a function returning a service status and an error.
-	Check(ctx context.Context) (HealthStatus, error)
+	Check(ctx context.Context) (HealthStatus, map[string]HealthStatus, error)
 }
 
 // HealthResponse is returned by the API when executing a health check.
@@ -51,6 +51,8 @@ type HealthResult struct {
 	Status HealthStatus `json:"status"`
 	// Message gives additional information on the health of a service.
 	Message string `json:"message"`
+	// Services is map of partitions by name with their individual health results.
+	Partitions map[string]HealthStatus `json:"partitions"`
 }
 
 type healthResource struct {
@@ -139,8 +141,9 @@ func (h *healthResource) check(request *restful.Request, response *restful.Respo
 			result := chanResult{
 				name: name,
 				HealthResult: HealthResult{
-					Status:  HealthStatusHealthy,
-					Message: "",
+					Status:     HealthStatusHealthy,
+					Message:    "",
+					Partitions: map[string]HealthStatus{},
 				},
 			}
 			defer func() {
@@ -148,7 +151,7 @@ func (h *healthResource) check(request *restful.Request, response *restful.Respo
 			}()
 
 			var err error
-			result.Status, err = healthCheck.Check(ctx)
+			result.Status, result.Partitions, err = healthCheck.Check(ctx)
 			if err != nil {
 				result.Message = err.Error()
 				h.log.Errorw("unhealthy service", "name", name, "status", result.Status, "error", err)
