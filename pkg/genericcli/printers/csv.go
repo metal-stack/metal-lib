b30873735d4ce9dd753789caca975494f377b56e
Gerrit <Gerrit91@users.noreply.github.com>
2024-03-11 13:39:36 +0100

    Replace CSV printer with more generic approach. (#131)

--- pkg/genericcli/printers/csv.go
+++ pkg/genericcli/printers/csv.go
@@ -1,38 +1,36 @@
 package printers
 
 import (
-	"encoding/csv"
+	"fmt"
 	"io"
 	"os"
-
-	"github.com/jszwec/csvutil"
+	"strings"
 )
 
 const defaultDelimiter = ';'
 
 type CSVPrinter struct {
-	c   *CSVPrinterConfig
-	out io.Writer
+	c *CSVPrinterConfig
 }
 
 type CSVPrinterConfig struct {
-	// AutoHeader will generate headers during print, default is go standard ("false")
-	AutoHeader bool
-	// Delimiter the char to separate the columns, default is ";"
-	Delimiter rune
+	// ToHeaderAndRows is called during print to obtain the headers and rows for the given data.
+	ToHeaderAndRows func(data any) ([]string, [][]string, error)
+	// NoHeaders will omit headers during pring when set to true
+	NoHeaders bool
 	// Out defines the output writer for the printer, will default to os.stdout
 	Out io.Writer
-	// Tag sets the struct field tag used for printing (default: json)
-	Tag string
+	// Delimiter the char to separate the columns, default is ";"
+	Delimiter rune
 }
 
 func NewCSVPrinter(config *CSVPrinterConfig) *CSVPrinter {
-	if config.Out == nil {
-		config.Out = os.Stdout
+	if config == nil {
+		config = &CSVPrinterConfig{}
 	}
 
-	if config.Tag == "" {
-		config.Tag = "json"
+	if config.Out == nil {
+		config.Out = os.Stdout
 	}
 
 	if config.Delimiter == 0 {
@@ -45,25 +43,28 @@ func NewCSVPrinter(config *CSVPrinterConfig) *CSVPrinter {
 }
 
 func (cp *CSVPrinter) WithOut(out io.Writer) *CSVPrinter {
-	cp.out = out
+	cp.c.Out = out
 
 	return cp
 }
 
 func (cp *CSVPrinter) Print(data any) error {
-	w := csv.NewWriter(cp.out)
-	w.Comma = cp.c.Delimiter
+	if cp.c.ToHeaderAndRows == nil {
+		return fmt.Errorf("missing to header and rows function in printer configuration")
+	}
 
-	enc := csvutil.NewEncoder(w)
-	enc.AutoHeader = cp.c.AutoHeader
-	enc.Tag = cp.c.Tag
-
-	err := enc.Encode(data)
+	headers, rows, err := cp.c.ToHeaderAndRows(data)
 	if err != nil {
 		return err
 	}
 
-	w.Flush()
+	if !cp.c.NoHeaders {
+		fmt.Fprintln(cp.c.Out, strings.Join(headers, string(cp.c.Delimiter)))
+	}
+
+	for _, row := range rows {
+		fmt.Fprintln(cp.c.Out, strings.Join(row, string(cp.c.Delimiter)))
+	}
 
 	return nil
 }
