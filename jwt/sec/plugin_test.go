a4c30342d3613a3c041579a60b48863933a045dc
Stefan Majer <stefan.majer@f-i-ts.de>
2024-03-25 10:35:57 +0100

    Migrate to jose v4

--- jwt/sec/plugin_test.go
+++ jwt/sec/plugin_test.go
@@ -6,7 +6,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/go-jose/go-jose/v3/jwt"
+	"github.com/go-jose/go-jose/v4/jwt"
 	"github.com/google/go-cmp/cmp"
 	"github.com/metal-stack/metal-lib/jwt/grp"
 	"github.com/metal-stack/security"
