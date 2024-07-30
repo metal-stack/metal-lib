100e8a963851d32e23157001c93816daabed5687
Valentin Knabel <dev@vknabel.com>
2024-07-30 10:43:25 +0200

    feat(auditing): disables typo tolerance
    meilisearch-go v0.27.1 fixes an issue where the typo tolerance could not be disabled. This should reduce the complexity of indexes but requires a settings migration period for every index

--- auditing/meilisearch.go
+++ auditing/meilisearch.go
@@ -445,14 +445,10 @@ func (a *meiliAuditing) migrateIndexSettings(index *meilisearch.Index) error {
 	}
 	diff := &meilisearch.Settings{}
 
-	// We'd like to disable the typo tolerance completely, but that's not possible, yet.
-	// TypoTolerance.Enabled is marked as omitempty and therefore prevents overriding true with false.
-	// https://github.com/meilisearch/meilisearch-go/issues/452
-	//
-	// if current.TypoTolerance != nil && current.TypoTolerance.Enabled != desired.TypoTolerance.Enabled {
-	// 	changesRequired = true
-	// 	diff.TypoTolerance = desired.TypoTolerance
-	// }
+	if current.TypoTolerance != nil && current.TypoTolerance.Enabled != desired.TypoTolerance.Enabled {
+		changesRequired = true
+		diff.TypoTolerance = desired.TypoTolerance
+	}
 
 	slices.Sort(current.SortableAttributes)
 	slices.Sort(desired.SortableAttributes)
