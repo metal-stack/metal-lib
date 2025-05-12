20feb43b7a3359625683755e0218f2ee454123de
Gerrit <tirreg91@googlemail.com>
2025-05-12 09:20:17 +0200

    Provide test.

--- auditing/async.go
+++ auditing/async.go
@@ -2,6 +2,7 @@ package auditing
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"time"
 
@@ -31,8 +32,10 @@ type (
 // If this is used it can occur that audit traces get lost in case the backend is not available for receiving the trace.
 // The advantage is that it does not block.
 func NewAsync(backend Auditing, log *slog.Logger, ac AsyncConfig) (Auditing, error) {
+	wrappedBackendType := fmt.Sprintf("%T", backend)
+
 	a := &asyncAuditing{
-		log:    log.WithGroup("auditing").With("audit-backend", "async"),
+		log:    log.WithGroup("auditing").With("audit-backend", "async", "wrapped-backend", wrappedBackendType),
 		config: &ac,
 		a:      backend,
 	}
@@ -50,23 +53,27 @@ func (a *asyncAuditing) Index(entry Entry) error {
 	go func() {
 		count := 0
 
+		log := a.log.With("entry-id", entry.Id, "retries", a.config.AsyncRetry, "backoff", a.config.AsyncBackoff.String())
+
 		for {
+			log.Debug("async index", "count", count)
+
 			err := a.a.Index(entry)
 			if err == nil {
 				return
 			}
 
-			if count > a.config.AsyncRetry {
-				a.log.Error("maximum amount of retries reached for sending event to splunk, giving up", "retries", a.config.AsyncRetry, "entry-id", entry.Id, "error", err)
+			if count >= a.config.AsyncRetry {
+				log.Error("maximum amount of retries reached for sending event to splunk, giving up", "error", err)
 				return
 			}
 
 			count++
 
-			a.log.Error("async indexing failed, retrying", "retries", a.config.AsyncRetry, "backoff", a.config.AsyncBackoff.String(), "error", err)
+			log.Error("async indexing failed, retrying", "error", err)
 			time.Sleep(*a.config.AsyncBackoff)
 
-			return
+			continue
 		}
 	}()
 
---
+++ auditing/async_test.go
@@ -0,0 +1,129 @@
+package auditing
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_asyncAuditing_Index(t *testing.T) {
+	tests := []struct {
+		name         string
+		asyncRetry   int
+		asyncBackoff time.Duration
+
+		idxFn func(count int) error
+
+		wantCount   int
+		wantTimeout bool
+	}{
+		{
+			name:         "index without error",
+			asyncRetry:   0,
+			asyncBackoff: 5 * time.Millisecond,
+			idxFn: func(_ int) error {
+				return nil
+			},
+			wantCount:   0,
+			wantTimeout: false,
+		},
+		{
+			name:         "index with error",
+			asyncRetry:   0,
+			asyncBackoff: 5 * time.Millisecond,
+			idxFn: func(_ int) error {
+				return errors.New("test backend error")
+			},
+			wantCount:   1,
+			wantTimeout: true,
+		},
+		{
+			name:         "retry does work",
+			asyncRetry:   3,
+			asyncBackoff: 5 * time.Millisecond,
+			idxFn: func(count int) error {
+				switch count {
+				case 0, 1, 2:
+					return errors.New("test backend error")
+				default:
+					return nil
+				}
+			},
+			wantCount:   3,
+			wantTimeout: false,
+		},
+		{
+			name:         "giving up on too many retries",
+			asyncRetry:   3,
+			asyncBackoff: 5 * time.Millisecond,
+			idxFn: func(count int) error {
+				switch count {
+				case 0, 1, 2, 3:
+					return errors.New("test backend error")
+				default:
+					return nil
+				}
+			},
+			wantCount:   4,
+			wantTimeout: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			done := make(chan bool)
+			defer close(done)
+
+			backend := &testBackend{idxFn: tt.idxFn, done: done}
+
+			async, err := NewAsync(backend, slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})), AsyncConfig{
+				AsyncRetry:   tt.asyncRetry,
+				AsyncBackoff: &tt.asyncBackoff,
+			})
+			require.NoError(t, err)
+
+			err = async.Index(Entry{Id: "test"})
+			require.NoError(t, err)
+
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+			defer cancel()
+
+			select {
+			case <-done:
+				require.False(t, tt.wantTimeout, "finished but timeout was expected")
+			case <-ctx.Done():
+				require.True(t, tt.wantTimeout, "unexpected timeout occurred")
+			}
+
+			assert.Equal(t, tt.wantCount, backend.count)
+		})
+	}
+}
+
+type testBackend struct {
+	done  chan bool
+	count int
+	idxFn func(count int) error
+}
+
+func (t *testBackend) Index(e Entry) error {
+	if t.idxFn != nil {
+		if err := t.idxFn(t.count); err != nil {
+			t.count++
+			return errors.New("test backend error")
+		}
+	}
+
+	t.done <- true
+
+	return nil
+}
+
+func (t *testBackend) Search(ctx context.Context, filter EntryFilter) ([]Entry, error) {
+	panic("not required")
+}
