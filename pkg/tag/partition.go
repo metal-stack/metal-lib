0253617526c39cacad7ba5a5cbd5d255c2adb22f
thomas <thomas.heinel@f-i-ts.de>
2025-09-10 10:05:37 +0200

    reintroduce PartitionTypeSingleZone and PartitionTypeRegionalAutospread

--- pkg/tag/partition.go
+++ pkg/tag/partition.go
@@ -3,4 +3,10 @@ package tag
 const (
 	// PartitionType describes a type of a partition, i.e. a partition that is located in a single zone inside a region or a partition which spreads machines across a region using the metal-stack rack spreading feature.
 	PartitionType = "partition.metal-stack.io/type"
+
+	// PartitionTypeSingleZone describes a partition that is located in a single zone inside a region.
+	PartitionTypeSingleZone         = "single-zone"
+
+	// PartitionTypeRegionalAutospread describes a partition, which spreads machines across a region using the metal-stack rack spreading feature.
+	PartitionTypeRegionalAutospread = "regional-autospread"
 )
