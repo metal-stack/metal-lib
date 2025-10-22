07041443fc74b2a0437a3a9cce3e5ea78c53993e
Philipp Mukosey <philipp.mukosey@x-cellent.com>
2025-10-22 13:24:04 +0200

    Add default templates for context subcommands

--- pkg/cmd/context.go
+++ pkg/cmd/context.go
@@ -1,34 +1,96 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+)
 
-type ContextConfig struct {
+type CmdConfig struct {
+	Use               string
 	ValidArgsFunction cobra.CompletionFunc
 	Example           string
 	RunE              func(cmd *cobra.Command, args []string) error
 	Short, Long       string
+	Aliases           []string
 }
 
-func NewContextCmd(c *ContextConfig) *cobra.Command {
+func ContextBaseCmd(c *CmdConfig) *cobra.Command {
+	if c.Use == "" {
+		c.Use = "context <name>"
+	}
 	if c.Short == "" {
-		c.Short = "Manage contexts"
+		c.Short = "manage cli contexts"
 	}
 	if c.Long == "" {
-		c.Long = "context defines the backend to talks to. You can switch back and forth with \"-\" as a shortcut to the last used context."
+		c.Long = "context defines the backend to talk to. You can switch back and forth with \"-\" as a shortcut to the last used context."
+	}
+	if c.Aliases == nil {
+		c.Aliases = []string{"ctx"}
 	}
 	if c.Example == "" {
 		c.Example = "Please refer to the documentation for examples on how to use contexts."
 	}
 
-	contextCmd := &cobra.Command{
-		Use:               "context <name>",
-		Aliases:           []string{"ctx"},
-		Short:             c.Short,
-		Long:              c.Long,
-		ValidArgsFunction: c.ValidArgsFunction,
-		Example:           c.Example,
-		RunE:              c.RunE,
+	return toCommand(c)
+}
+
+func ContextAddCmd(c *CmdConfig) *cobra.Command {
+	if c.Use == "" {
+		c.Use = "add <context-name>"
+	}
+	if c.Short == "" {
+		c.Short = "add a cli context"
+	}
+	if c.Aliases == nil {
+		c.Aliases = []string{"create"}
 	}
 
-	return contextCmd
+	return toCommand(c)
+}
+
+func ContextRemoveCmd(c *CmdConfig) *cobra.Command {
+	if c.Use == "" {
+		c.Use = "remove <context-name>"
+	}
+	if c.Short == "" {
+		c.Short = "remove a cli context"
+	}
+	if c.Aliases == nil {
+		c.Aliases = []string{"delete", "rm"}
+	}
+
+	return toCommand(c)
+}
+
+func ContextUpdateCmd(c *CmdConfig) *cobra.Command {
+	if c.Use == "" {
+		c.Use = "update <context-name>"
+	}
+	if c.Short == "" {
+		c.Short = "update a cli context"
+	}
+
+	return toCommand(c)
+}
+
+func ContextListCmd(c *CmdConfig) *cobra.Command {
+	if c.Short == "" {
+		c.Short = "list the configured cli contexts"
+	}
+	if c.Use == "" {
+		c.Use = "list"
+	}
+
+	return toCommand(c)
+}
+
+func toCommand(c *CmdConfig) *cobra.Command {
+	return &cobra.Command{
+		Use:               c.Use,
+		Aliases:           c.Aliases,
+		Short:             c.Short,
+		Long:              c.Long,
+		Example:           c.Example,
+		ValidArgsFunction: c.ValidArgsFunction,
+		RunE:              c.RunE,
+	}
 }
