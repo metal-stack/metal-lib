4c8f1c341267f5bce3574594c5ed3e3243b048bf
Stefan Majer <stefan.majer@f-i-ts.de>
2026-04-09 09:29:18 +0200

    fix link

--- pkg/tag/machine.go
+++ pkg/tag/machine.go
@@ -2,27 +2,27 @@ package tag
 
 const (
 	// MachineID tag to store machine ID
-	// Deprecated use github.com/metal-stack/go/tag/MachineID
+	// Deprecated use github.com/metal-stack/api/go/tag/MachineID
 	MachineID = "machine.metal-stack.io/id"
 	// MachineName tag to store machine name
-	// Deprecated use github.com/metal-stack/go/tag/MachineName
+	// Deprecated use github.com/metal-stack/api/go/tag/MachineName
 	MachineName = "machine.metal-stack.io/name"
 	// MachineDescription tag to store machine description
-	// Deprecated use github.com/metal-stack/go/tag/MachineDescription
+	// Deprecated use github.com/metal-stack/api/go/tag/MachineDescription
 	MachineDescription = "machine.metal-stack.io/description"
 	// MachineProject tag to store the project where the machine belongs to
-	// Deprecated use github.com/metal-stack/go/tag/MachineProject
+	// Deprecated use github.com/metal-stack/api/go/tag/MachineProject
 	MachineProject = "machine.metal-stack.io/project"
 	// MachineTenant tag to store the tenant the machine belongs to
-	// Deprecated use github.com/metal-stack/go/tag/MachineTenant
+	// Deprecated use github.com/metal-stack/api/go/tag/MachineTenant
 	MachineTenant = "machine.metal-stack.io/tenant"
 	// MachineNetworkPrimaryASN tag to store the primary BGP ASN the machine announces.
-	// Deprecated use github.com/metal-stack/go/tag/MachineNetworkPrimaryASN
+	// Deprecated use github.com/metal-stack/api/go/tag/MachineNetworkPrimaryASN
 	MachineNetworkPrimaryASN = "machine.metal-stack.io/network.primary.asn"
 	// MachineRack tag to store the rack that this machine is placed in.
-	// Deprecated use github.com/metal-stack/go/tag/MachineRack
+	// Deprecated use github.com/metal-stack/api/go/tag/MachineRack
 	MachineRack = "machine.metal-stack.io/rack"
 	// MachineChassis tag to store the machine chassis.
-	// Deprecated use github.com/metal-stack/go/tag/MachineChassis
+	// Deprecated use github.com/metal-stack/api/go/tag/MachineChassis
 	MachineChassis = "machine.metal-stack.io/chassis"
 )
